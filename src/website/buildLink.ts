6a9736dddc3c761db2e1656d7975e54156a7e47e
Nicolas Assouad <nicolas@assouad.com>
2024-04-23 23:14:54 +0200

    Change email address

--- src/website/buildLink.ts
+++ src/website/buildLink.ts
@@ -1,18 +1,14 @@
 import { Language } from "./wordings";
 
-// To test locally
-const GITHUB_PAGES_URL = "";
-//const GITHUB_PAGES_URL = "https://fondation451.github.io/nicolasassouad";
-
 export const buildLink = (path: string, language: Language): string => {
   switch (language) {
     case "en":
-      return `${GITHUB_PAGES_URL}${path}`;
+      return path;
     default:
-      return `${GITHUB_PAGES_URL}/${language}${path}`;
+      return `/${language}${path}`;
   }
 };
 
 export const buildPublicLink = (path: string): string => {
-  return `${GITHUB_PAGES_URL}${path}`;
+  return path;
 };
